952cac38fc6979025a302f9c0e3c56486679f85d
laravelseniorpro3000 <navier.albara@fileexp.com>
2025-02-02 06:25:42 +0400

    create Document model and migration

---
+++ database/migrations/2025_02_02_010610_create_documents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('Название документа');
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('file_name')->comment('Название файла в системе');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
